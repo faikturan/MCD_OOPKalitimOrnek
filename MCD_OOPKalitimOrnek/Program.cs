58bb3e5d8f9b168d044531bd6bb36e683b4d83f3
Faik Turan <turanfaik@hotmail.com>
2021-12-03 16:07:01 +0300

    code changed

--- MCD_OOPKalitimOrnek/Program.cs
+++ MCD_OOPKalitimOrnek/Program.cs
@@ -10,6 +10,31 @@ namespace MCD_OOPKalitimOrnek
     {
         static void Main(string[] args)
         {
+            Alman kisi = new Alman();
+            kisi.Ad = "Thomas";
+            kisi.Soyad = "Müller";
+            Console.WriteLine(kisi.Ad + " şöyle dedi.");
+            kisi.Selamlasma();
+
+            Turk kisi2 = new Turk();
+            kisi2.Ad = "Faik";
+            kisi2.Soyad = "Turan";
+            Console.WriteLine(kisi2.Ad + " şöyle dedi.");
+            kisi2.Selamlasma();
+
+            Fasli kisi3 = new Fasli();
+            kisi3.Ad = "Maryam";
+            kisi3.Soyad = "Abdollah";
+            Console.WriteLine(kisi3.Ad + " şöyle dedi.");
+            kisi3.Selamlasma();
+
+            Ingiliz kisi4 = new Ingiliz();
+            kisi4.Ad = "Jennifer";
+            kisi4.Soyad = "Lopez";
+            Console.WriteLine(kisi4.Ad + " şöyle dedi.");
+            kisi4.Selamlasma();
+
+            Console.ReadKey();
         }
     }
 }
